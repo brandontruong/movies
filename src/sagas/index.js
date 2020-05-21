b932f58faef3be94b4122f28dcc4b4cd70aae8ec
Brandon Truong <brandon.truong@sca.com.au>
2020-05-21 22:38:58 +1000

    added https

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -2,8 +2,7 @@ import { fork } from 'redux-saga/effects';
 import DataSourceSaga from './DataSourceSaga';
 import create from '../services/Api';
 
-// const apiService = create('http://localhost:9000');
-const apiService = create('http://www.omdbapi.com/?apikey=2aa43e7');
+const apiService = create('https://www.omdbapi.com/?apikey=2aa43e7');
 
 // start the daemons
 export default function* root() {
